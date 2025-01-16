b7ccb9d04fa865d9e1c69069ea3bde6ee25c97f9
williamdubgod <william_vulcano@hotmail.com>
2025-01-16 19:33:36 -0300

    atualização do logo e nome da página

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/logo.png'
+import logo from '../../logo.png'
 
 export default function Header() {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Aescult/', // Certifique-se de que está assim
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
